49e1ac1c3642e6da57308c1e22d3d9f527c71a51
Abhinav <abhinavraut420@gmail.com>
2019-05-09 21:16:22 +0530

    added try except block

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import time
 import re
 import requests
 import schedule
+import traceback
 
 print('my twitter bot')
 
@@ -19,7 +20,7 @@ api = tweepy.API(auth,
 
 mentions = api.mentions_timeline()
 
-FILE_NAME = 'last_seen.txt'
+FILE_NAME = 'last_seen1.txt'
 USERS_FILE = 'users.txt'
 
 
@@ -37,9 +38,9 @@ def store_last_seen_id(last_seen_id, file_name):
     return
 
 
-def storeUser(userID, city, file_name):
-    f_write = open(file_name, 'a')
-    f_write.write(str(userID + ',' + city + '\n'))
+def storeuser(userID, city, mentionid):
+    f_write = open(USERS_FILE, 'a')
+    f_write.write(str(userID + ',' + city + ',' + str(mentionid) + '\n'))
     f_write.close()
     return
 
@@ -51,71 +52,76 @@ def getWeather(city):
 
 
 def displayWeather():
-    print('Retrieving and replying to tweets...')
+    try:
+        print('Retrieving and replying to tweets...')
 
-    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+        last_seen_id = retrieve_last_seen_id(FILE_NAME)
 
-    mentions = api.mentions_timeline(last_seen_id,
-                                     tweet_mode='extended')
+        mentions = api.mentions_timeline(last_seen_id,
+                                         tweet_mode='extended')
 
-    for mention in reversed(mentions):
-        print(str(mention.id) + ' - ' + mention.full_text)
-        last_seen_id = mention.id
-        store_last_seen_id(last_seen_id, FILE_NAME)
-        if '#' in mention.full_text.lower():
-            if '#unsub' not in mention.full_text.lower():
-                s = mention.full_text.lower()
-                tags = re.findall(r"#(\w+)", s)
+        for mention in reversed(mentions):
+            print(str(mention.id) + ' - ' + mention.full_text)
+            last_seen_id = mention.id
+            store_last_seen_id(last_seen_id, FILE_NAME)
+            if '#' in mention.full_text.lower():
+                if '#unsub' not in mention.full_text.lower():
+                    s = mention.full_text.lower()
+                    tags = re.findall(r"#(\w+)", s)
 
-                if len(tags) is 1:
-                    print(tags)
-                    x = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={tags[0]}'
-                                     f'&APPID=YOURKEYHERE').json()
-                    if x['cod'] == 200:
-                        print('200')
-                        temperature = getWeather(tags[0])
-                        degree = '°C'
-                        if mention.user.screen_name in open(USERS_FILE).read():
-                            print("User already present in file")
-                            with open(USERS_FILE, 'r') as file:
-                                lines = file.readlines()
-                            # now we have an array of lines.
+                    if len(tags) is 1:
+                        x = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={tags[0]}'
+                                         f'&APPID=YOURKEYHERE').json()
+                        if x['cod'] == 200:
+                            temperature = getWeather(tags[0])
+                            degree = '°C'
+                            if mention.user.screen_name in open(USERS_FILE).read():
+                                print("User already present in file")
+                                with open(USERS_FILE, 'r') as file:
+                                    lines = file.readlines()
+                                # now we have an array of lines.
 
-                            for idx, val in enumerate(lines):
-                                if mention.user.screen_name in val:
-                                    lines[idx] = f'{mention.user.screen_name},{tags[0]}\n'
-                            with open(USERS_FILE, 'w') as file:
-                                file.writelines(lines)
+                                for idx, val in enumerate(lines):
+                                    if mention.user.screen_name in val:
+                                        lines[idx] = f'{mention.user.screen_name},{tags[0]},{mention.id}\n'
+                                with open(USERS_FILE, 'w') as file:
+                                    file.writelines(lines)
+                            else:
+                                print('tweeting back to user')
+                                api.update_status(f'@{mention.user.screen_name} Weather for {tags[0]} is '
+                                                  f'{temperature}{degree}, You have successfully subscribed, '
+                                                  f'you will receive 1 tweet daily with the weather details'
+                                                  f'. Use #unsub to unsubscribe anytime.', mention.id)
+                                storeuser(mention.user.screen_name, tags[0], mention.id)
+                                # my_list = line.split(",")
                         else:
-                            print('tweeting back to user')
-                            api.update_status(f'@{mention.user.screen_name} Weather for {tags[0]} is '
-                                              f'{temperature}{degree}, You have successfully subscribed.'
-                                              f' Use #unsub to unsubscribe', mention.id)
-                            storeUser(mention.user.screen_name, tags[0], USERS_FILE)
-                            # my_list = line.split(",")
-                    else:
-                        print('city not found')
-            else:
-                with open(USERS_FILE, "r") as infile:
-                    lines = infile.readlines()
+                            print('city not found')
+                else:
+                    with open(USERS_FILE, "r") as infile:
+                        lines = infile.readlines()
 
-                    for pos, line in enumerate(lines):
-                        if mention.user.screen_name in line:
-                            lines[pos] = ''
-                    with open(USERS_FILE, 'w') as file:
-                        file.writelines(lines)
-
+                        for pos, line in enumerate(lines):
+                            if mention.user.screen_name in line:
+                                lines[pos] = ''
+                        with open(USERS_FILE, 'w') as file:
+                            file.writelines(lines)
+                    api.update_status(f'@{mention.user.screen_name} Unsubscribed!', mention.id)
+    except Exception as ex:
+        traceback.print_exc()
 
 def scheduledweather():
-    print('in scheduled weather')
-    with open(USERS_FILE) as f:
-        for line in f:
-            my_list = line.split(",")
-            degree = '°C'
-            temperature = getWeather(my_list[1])
-            city = my_list[1].replace('\n', '')
-            api.update_status(f'@{my_list[0]} Weather for {city} is {temperature}{degree}')
-            print('sent!!')
+    try:
+        print('in scheduled weather')
+        with open(USERS_FILE) as f:
+            for line in f:
+                my_list = line.split(",")
+                degree = '°C'
+                temperature = getWeather(my_list[1])
+                mentionid = my_list[2].replace('\n', '')
+                api.update_status(f'@{my_list[0]} Weather for {my_list[1]} is {temperature}{degree}', mentionid)
+                print('sent!!')
+    except:
+        print('Error')
 
 
 # schedule.every(10).minutes.do(job)
@@ -123,13 +129,15 @@ def scheduledweather():
 # schedule.every(5).to(10).minutes.do(job)
 # schedule.every().monday.do(job)
 # schedule.every().wednesday.at("13:15").do(job)
-schedule.every().day.at("13:55").do(scheduledweather)
+schedule.every().day.at("11:00").do(scheduledweather)
 schedule.every().minute.at(":30").do(displayWeather)
+
 
 while True:
     try:
         schedule.run_pending()
         time.sleep(1)
+
     except tweepy.TweepError:
         time.sleep(2)
         continue
